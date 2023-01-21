ce6f88baa35c512382d9b94e8307d6d8cd7f8e2c
Abdullahinur Ahmed Abdullahi <ab2d222@gmail.com>
2023-01-21 17:21:39 +0300

    add other css and change header bg

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,35 @@
-// alert("Hello world!");
+
+$(document).ready(function(){
+    // your script here
+    // alert("Hello world!");
+
+// COLOR CONVERSION FOR NAVBAR
+var counter = 0;
+var colors = [
+    "#eeeeee",
+    "#00ff00",
+    "#ff0000",
+    "#000000"];
+
+var $div = $('a#my-header-style');
+console.log($div);
+var interval;
+$('a#my-header-style').mouseenter(function () {
+    interval = window.setInterval(changeColor, 10); //set the interval of 1 sec for image to change while hovered.
+
+}).mouseleave(function () {
+    // alert("I was clicked");
+    window.clearInterval(interval); //clear the interval on mouseOut.
+});
+
+function changeColor() {
+    var color = colors.shift();
+    colors.push(color);
+    $div.css({
+        "color": color
+    });
+    console.log(colors + "I've changed color to " + colors);
+
+}
+ });
+ 
