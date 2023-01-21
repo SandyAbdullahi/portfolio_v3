f891a69a24e266861a9d3a1982ae49f1f298e543
Abdullahinur Ahmed Abdullahi <ab2d222@gmail.com>
2023-01-21 18:13:07 +0300

    add script to generate logo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,3 +33,32 @@ function changeColor() {
 }
  });
  
+
+//  LOGO
+   // Get the canvas element
+   var canvas = document.getElementById("myCanvas");
+   // Check if the canvas is supported on the browser
+   if (canvas.getContext) {
+       // Get the 2D rendering context
+       var ctx = canvas.getContext("2d");
+       // Draw a rectangle
+       ctx.fillStyle = "#5e5e5e";
+       ctx.fillRect(0, 0, 50, 50);
+       // Draw a circle
+       ctx.beginPath();
+       ctx.arc(25, 25, 20, 0, 2 * Math.PI);
+       ctx.fill();
+       // Draw lines
+       ctx.beginPath();
+       ctx.moveTo(25, 0);
+       ctx.lineTo(25, 50);
+       ctx.moveTo(0, 25);
+       ctx.lineTo(50, 25);
+       ctx.stroke();
+       // Draw a cloud
+       ctx.beginPath();
+       ctx.arc(35, 10, 10, 0, Math.PI, true);
+       ctx.arc(15, 10, 10, 0, Math.PI, true);
+       ctx.arc(25, 20, 15, 0, Math.PI, true);
+       ctx.fill();
+   }
